c3fa06bb4811d3c397bc81ba539281c7b68c850a
hudongdong <924859690@qq.com>
2019-01-09 23:29:03 +0800

    添加qq登录

--- src/main/java/com/hu/security/core/social/qq/connet/QQServiceProvider.java
+++ src/main/java/com/hu/security/core/social/qq/connet/QQServiceProvider.java
@@ -1,25 +1,29 @@
-//package com.hu.security.core.social.qq.connet;
-//
-//import com.hu.security.core.social.qq.api.QQ;
-//import com.hu.security.core.social.qq.api.QQImpl;
-//import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
-//import org.springframework.social.oauth2.OAuth2Operations;
-//
-//public class QQServiceProvider extends AbstractOAuth2ServiceProvider<QQ> {
-//
-//    private String appId;
-//
-//    /**
-//     * Create a new {@link OAuth2ServiceProvider}.
-//     *
-//     * @param oauth2Operations the OAuth2Operations template for conducting the OAuth 2 flow with the provider.
-//     */
-//    public QQServiceProvider(OAuth2Operations oauth2Operations) {
-//        super(oauth2Operations);
-//    }
-//
-//    @Override
-//    public QQ getApi(String accessToken) {
-//        return new QQImpl(accessToken, appId);
-//    }
-//}
+package com.hu.security.core.social.qq.connet;
+
+import com.hu.security.core.social.qq.api.QQ;
+import com.hu.security.core.social.qq.api.QQImpl;
+import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
+import org.springframework.social.oauth2.OAuth2Template;
+
+public class QQServiceProvider extends AbstractOAuth2ServiceProvider<QQ> {
+
+    private String appId;
+
+    private static final String URL_AUTHORIZE = "https://graph.qq.com/oauth2.0/authorize";
+
+    private static final String URL_ACCESS_TOKEN = "https://graph.qq.com/oauth2.0/token";
+
+    /**
+     * Create a new {@link OAuth2ServiceProvider}.
+     *
+     * @param oauth2Operations the OAuth2Operations template for conducting the OAuth 2 flow with the provider.
+     */
+    public QQServiceProvider(String appId, String appSecret) {
+        super(new OAuth2Template(appId, appSecret, URL_AUTHORIZE, URL_ACCESS_TOKEN));
+    }
+
+    @Override
+    public QQ getApi(String accessToken) {
+        return new QQImpl(accessToken, appId);
+    }
+}
